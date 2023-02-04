545f10767edd49bd41bf0771189d1cf6091c921e
hannapodeszwa <hanna.podeszwa@gmail.com>
2023-02-04 19:17:39 +0100

    init

--- BookCatalog/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace BookCatalog.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
